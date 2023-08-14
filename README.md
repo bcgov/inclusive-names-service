213da8e57488c66799dc886c8918b8caeaa323c7
Dunham-Wilkie <Mike.Dunham-Wilkie@gov.bc.ca>
2023-08-14 10:36:45 -0700

    added BCSans glyph unicode details spreadsheet

---
+++ README.md
@@ -0,0 +1,20 @@
+# Techniques for Dealing with Unicode Data
+This repository includes code and tips that will be useful to systems developers and maintainers who need to ensure that their computer systems can properly input, store, process, and display/export Unicode characters.
+
+### Programming Languages
+[Programming Languages](.\programming_languages\README.md)
+
+### Database Management Systems
+[Databases](.\databases\README.md)
+
+### Some Test Data
+[Test Data](.\test_data\README.md)
+
+### File Formats
+[File Formats](.\file_formats\README.md)
+
+### Data Transfer Protocols
+[Data Transfer Protocols](.\data_transfer_protocols\README.md)
+
+### Data Flow Analysis Primer and Example
+[Data Flow Analysis](.\data_flow_analysis\README.md)
---
+++ test_data/check_bc_sans.py
@@ -0,0 +1,23 @@
+from itertools import chain
+import sys
+
+from fontTools.ttLib import TTFont
+from fontTools.unicode import Unicode
+
+
+with TTFont(
+    sys.argv[1], 0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1
+) as ttf:
+    chars = chain.from_iterable(
+        [y + (Unicode[y[0]],) for y in x.cmap.items()] for x in ttf["cmap"].tables
+    )
+    outfile = 'bcsans2.out'
+    f = open(outfile,"w",encoding="utf-8")
+    if len(sys.argv) == 2:  # print all code points
+        for c in chars:
+            frame = b"e"
+            frame += (chr(c[0]).encode('utf-8'))
+            mychar = frame.decode('utf-8')
+            strval = str(c[0])+','+c[1]+','+c[2] + ',' + str((chr(c[0])).encode('utf-8')) + ',' + chr(c[0])  +  ',' + '\n'
+            f.write(strval)
+            
