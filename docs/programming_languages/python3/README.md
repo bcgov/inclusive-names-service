587d1258b399665d5d6e8dd6993779837cf53cc9
Dunham-Wilkie <Mike.Dunham-Wilkie@gov.bc.ca>
2024-04-12 19:32:39 -0700

    updated python example

--- docs/programming_languages/python3/README.md
+++ docs/programming_languages/python3/README.md
@@ -1,4 +1,4 @@
-This example requires regex, installing them or setting up a virtual environment is left as an exercise to the reader
+This example requires the regex, unicodedata, anyascii, and grapheme libraries. 
 
 A note on graphemes
 Depends on what you are looking for. graphemes() will return what users would perceive as single characters, even though they may contain more than one codepoint; for example a letter combined with an accent mark is a single grapheme. This is not the case with split().
@@ -6,5 +6,5 @@ Depends on what you are looking for. graphemes() will return what users would pe
 Consider a + ◌́ . In this example, split() will return the two codepoints as separate characters whereas graphemes() will return a single character.
 
 ## Gotchas
-- prefacing a sting with a u will switch it to unicode
+- prefacing a string with a u will switch it to unicode
 - for graphemes you need the regex \X
--- docs/programming_languages/python3/utf8.py
+++ docs/programming_languages/python3/utf8.py
@@ -1,35 +1,57 @@
-#! /usr/local/bin/python3
-
 import regex
+import unicodedata
+import grapheme
+from anyascii import anyascii
 
-output = open("output.txt","w")
+output = open("output.txt","w",encoding="utf-8")
 with open("../utf8.txt",encoding="utf-8",mode="r") as exampleFile:
     for line in exampleFile:
         # Has new line from file input ensure we write out the proper encoded version
-        output.write("String: "+line)
+        output.write("\n\nString: "+line)
         print("String: "+line[:-1])
+        
+        output.write("anyascii version: "+anyascii(line) )
+        print("anyascii version: "+anyascii(line))
 
         output.write("String type: "+str(type(line))+"\n")
-        print("String type: "+str(type(line)))
+        print("String type: "+str(type(line))+"\n")
 
         # Note that if you preface a string with u it becomes unicode instead of ascii
-        strList = u", ".join(list(line))
-        output.write("List: " + strList)
-        print("List: " + strList[:-1])
+        # Normalize the string 4 ways: NFC, NFKC, NFD, and NFKD
+        NFCStrList = u"".join(list(unicodedata.normalize('NFC',line)))
+        NFDStrList = u"".join(list(unicodedata.normalize('NFD',line)))
+        NFKCStrList = u"".join(list(unicodedata.normalize('NFKC',line)))
+        NFKDStrList = u"".join(list(unicodedata.normalize('NFKD',line)))
+        print('NFC character list: '+u", ".join(NFCStrList))
+        print('NFD character list: '+u", ".join(NFDStrList))
+        print('NFKC character list: '+u", ".join(NFKCStrList))
+        print('NFKD character list: '+u", ".join(NFKDStrList))
+        output.write('NFC character list: '+u", ".join(NFCStrList))
+        output.write('NFD character list: '+u", ".join(NFDStrList))
+        output.write('NFKC character list: '+u", ".join(NFKCStrList))
+        output.write('NFKD character list: '+u", ".join(NFKDStrList))
 
-        output.write("Length: "+str(len(line))+"\n")
-        print("Length: "+str(len(line)))
+        output.write("Length - NFC: "+str(len(NFCStrList))+"\n")
+        print("Length - NFC: "+str(len(NFCStrList)-1)+"\n")
+        output.write("Length - NFD: "+str(len(NFDStrList))+"\n")
+        print("Length - NFD: "+str(len(NFDStrList)-1)+"\n")
 
-        output.write("Seventh character: "+ line[6]+"\n")
-        print("Seventh character: " + line[6])
+        output.write("Seventh character (NFC): "+ NFCStrList[6]+"\n")
+        print("Seventh character (NFC): "+ NFCStrList[6]+"\n")
+        output.write("Seventh character (NFD): "+ NFDStrList[6]+"\n")
+        print("Seventh character (NFD): "+ NFDStrList[6]+"\n")
 
         graphemeArr = regex.findall(r'\X', line, regex.U)
         graphemes = u", ".join(graphemeArr)
         output.write("Grapheme(s): " + graphemes)
         print("Grapheme(s): " + graphemes, end = '')
-
-        output.write("Grapheme(s) Length: " + str(len(graphemeArr)) + "\n\n")
-        print("Grapheme(s) Length: " + str(len(graphemeArr)) + "\n")
+        
+        output.write("Grapheme(s) Length: " + str(len(graphemeArr)-1) + "\n")
+        print("Grapheme(s) Length: " + str(len(graphemeArr)-1) + "\n")
+        
+        output.write("Sixth Grapheme: "+grapheme.slice(line, start=5, end=6) + "\n")
+        print("Sixth Grapheme: "+grapheme.slice(line, start=5, end=6) + "\n")
+        
 output.close()
 exampleFile.close()
 
